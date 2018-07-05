30220aaf5469bd432a4a05c1778a7d2ce48ebbc4
Terrance Corley <terrancecorley@outlook.com>
2018-07-04 18:59:39 -0700

    added main game interactivity

--- game.py
+++ game.py
@@ -61,9 +61,6 @@ def help_menu():
   print('- Good luck and have fun!')
   title_screen_selections()
 
-#### Game Functionality ####
-def start_game():
-
 
 
 
@@ -272,3 +269,61 @@ zonemap = {
     RIGHT: '',
   },
 }
+
+#### GAME INTERACTIVITY ####
+def print_location():
+  print('\n' + ('#' * (4 + len(myPlayer.location))))
+  print('# ' + myPlayer.location.upper() + ' #')
+  print('# ' + zonemap[myPlayer.location][DESCRIPTION] + ' #')
+  print('\n' + ('#' * (4 + len(myPlayer.location))))
+
+
+def prompt():
+  print('\n' + '============================')
+  print('What would you like to do?')
+  action = input('> ')
+  acceptable_actions = [
+    'move', 'go', 'travel', 'walk', 'quit',
+    'quit', 'examine', 'inspect', 'interact',
+    'look'
+  ]
+  while action.lower() not in acceptable_actions:
+    print('Unknown action, try again.\n')
+    action = input('> ')
+  if action.lower() == 'quit':
+    sys.exit()
+  elif action.lower() == ['move', 'go', 'travel', 'walk']:
+    player_move(action.lower())
+  elif action.lower() == ['examine', 'inspect', 'interact', 'look']:
+    player_examine(action.lower())
+
+def player_move(myAction):
+  ask = 'Where would you like to move to?\n'
+  dest = input(ask)
+  if dest in ['up', 'north']:
+    destination = zonemap[myPlayer.location][UP]
+    movement_handler(destination)
+  elif dest in ['down', 'south']:
+    destination = zonemap[myPlayer.location][DOWN]
+    movement_handler(destination)
+  elif dest in ['left', 'west']:
+    destination = zonemap[myPlayer.location][LEFT]
+    movement_handler(destination)
+  elif dest in ['right', 'east']:
+    destination = zonemap[myPlayer.location][RIGHT]
+    movement_handler(destination)
+
+def movement_handler(destination):
+  print('\n' + 'You have moved to the ' + destination + '.')
+  myPlayer.location = destination
+  print_location()
+
+def player_examine(action):
+  if zonemap[myPlayer.location][SOLVED]:
+    print('You have alread exhausted this zone.')
+  else:
+    print('You can trigger a puzzle here.')
+
+#### Game Functionality ####
+def start_game():
+  return
