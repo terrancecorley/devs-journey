9ec4ce271fa42080d15ef77bbb80a011d8147c22
Terrance Corley <terrancecorley@outlook.com>
2018-07-06 07:59:12 -0700

    added mr pretrov diaglogue

--- game.py
+++ game.py
@@ -110,8 +110,8 @@ zonemap = {
     RIGHT: 'a2',
   },
   'a2': {
-    ZONENAME: 'Town Hall',
-    DESCRIPTION: 'You enter the Heroes Town Hall.',
+    ZONENAME: 'Mr. Petrov - Lead Web Designer',
+    DESCRIPTION: 'You stand in front of a bright red door, you can hear the buzzing sound of what appears to be bad 90\'s techno music playing on the other side.',
     EXAMINATION: 'examine',
     SOLVED: False,
     UP: '',
@@ -185,7 +185,7 @@ zonemap = {
 def print_location():
   print('\n' + ('#' * (4 + len(myPlayer.location))))
   print('# ' + zonemap[myPlayer.location][ZONENAME].upper() + ' #')
-  print('# ' + zonemap[myPlayer.location][DESCRIPTION] + ' #')
+  print('# ' + zonemap[myPlayer.location][DESCRIPTION] + ' #\n')
   print('\n' + ('#' * (4 + len(myPlayer.location))))
 
 def new_line():
@@ -241,22 +241,58 @@ def movement_handler(destination):
     myPlayer.location = destination
     print_location()
 
+def text_speech2(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.03)
+
 def player_examine(action):
   if zonemap[myPlayer.location][SOLVED]:
     print('* A mysterious voice on the company loudspeaker yells *\nYou have already been through this door!! Why on earth would you ever want to go back?! Fool!!')
+  elif zonemap[myPlayer.location][ZONENAME] == 'Mr. Petrov - Lead Web Designer':
+    os.system('clear')
+    petrov1 = '(AMBIANCE): uhn tis uhn tis uhn tis uhn tis\n'
+    text_speech2(petrov1)
+    petrov2 = 'Mr. Petrov: Yas! Yas! Come on innnnnn, recruit!\n'
+    text_speech2(petrov2)
+    petrov3 = 'Terrance: ' + 'Umm, hi there it\'s nice to mee-\n'
+    text_speech2(petrov3)
+    petrov4 = 'Mr. Petrov: Hold on comrade! Hold on...\n'
+    text_speech2(petrov4)
+    petrov5 = 'Mr. Petrov: Before I even shake your hand, answer me this...\n'
+    text_speech2(petrov5)
+    petrov6 = 'Mr. Petrov: ...do you...like-a de technaw?\n'
+    text_speech2(petrov6)
+    petrov7 = 'Terrance: ' + '...the technaw? Do you mean techn-\n'
+    text_speech2(petrov7)
+    petrov8 = 'Mr. Pretov: Yas comrade! Thee techNAWWWWWW!\n'
+    text_speech2(petrov8)
+    petrov9 = 'Terrance: ' + 'I mean sure, techno is...alright.\n'
+    text_speech2(petrov9)
+    petrov10 = 'Mr. Pretov: Alright? ALRIGHT?! No, no, no. This will not work. Recruit, how can you nawt love et huh? What else are you going to say next? That you are still using PNG in place of SVG for your site logos?!\n'
+    text_speech2(petrov10)
+    petrov11 = 'Mr. Pretov: Wait...I know almost nothing about your design skills, comrade. How about you riddle me this...if I have a wonderful high res photo of me and all my friends outside the best technaw club in San Francisco...which image format should this photo be saved as?\n'
+    text_speech2(petrov11)
+    answer = input('> JPG, PNG, SVG, GIF?...')
+    if answer == 'JPG':
+      zonemap[myPlayer.location][SOLVED] = True
+      petrov12 = 'Mr. Pretov: HAAAAAYYYYY! Shots! Shots! Shots! Take a shot comrade, I knew you were a smart one!\n'
+      text_speech2(petrov12)
+    else: 'Mr. Pretov: That is wrong comrade...'
   else:
     print('You can trigger a puzzle here.')
 
 def player_options(action):
   os.system('clear')
-  print('These commands are available: "move", "examine", "quit"')
+  print('These commands are available: "move", "knock", "quit"')
 
 #### Game Functionality ####
 def text_speech(text):
   for character in text:
     sys.stdout.write(character)
     sys.stdout.flush()
-    # time.sleep(0.03)
+    time.sleep(0.03)
 
 def enter_to_continue():
   input('> Press "Enter" to continue')
@@ -292,15 +328,7 @@ def setup_game():
         myPlayer.job = player_job
   
   #### PLAYER STATS ####
-  if myPlayer.job is 'front-end developer':
-    self.hp = 120
-    self.mp = 20
-  elif myPlayer.job is 'back-end developer':
-    self.hp = 40
-    self.mp = 120
-  elif myPlayer.job is 'web designer':
-    self.hp = 60
-    self.mp = 60
+  # Build stats here
 
   #### INTRODUCTION #### 
   myPlayer.name = player_name
